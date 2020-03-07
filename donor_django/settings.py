e5e9af1aec2dd8848ab1bfa0af2fd54efa2efb08
SRBond <skyler.bond7@gmail.com>
2020-03-07 11:05:42 -0800

    deployed, still in debug...

--- donor_django/settings.py
+++ donor_django/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 import os
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.getenv('SECRET_KEY', 0)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
