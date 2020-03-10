d2d1634d428d419b8c342c4df662f03ffe6ac7aa
SRBond <skyler.bond7@gmail.com>
2020-03-10 10:07:00 -0700

    fix token timeout

--- donor_django/settings.py
+++ donor_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -56,7 +57,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=180),
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=180),
 }
 
 MIDDLEWARE = [
