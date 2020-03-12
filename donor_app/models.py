cdf5ef6c81889136baf7d682ff8345ac785647cd
SRBond <skyler.bond7@gmail.com>
2020-03-12 12:33:30 -0700

    allow fields to be blank in Donor

--- donor_app/models.py
+++ donor_app/models.py
@@ -5,17 +5,17 @@ from django.dispatch import receiver
 
 class Donor(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='donors', default=5)
-    botsteward = models.CharField(max_length=100, default="Gretchen Bond")
+    botsteward = models.CharField(max_length=100, default="Gretchen Bond", blank=True)
     orgName = models.CharField(max_length=100, default="")
-    lastname = models.CharField(max_length=100, default="")
-    phone = models.CharField(max_length=100, default="")
-    email = models.CharField(max_length=100, default="")
-    paymentnum = models.CharField(max_length=100, default="")
-    yeartotal = models.CharField(max_length=100, default="")
-    lastgift = models.CharField(max_length=100, default="")
-    lastgiftdate = models.CharField(max_length=100, default="")
-    nextlastgift = models.CharField(max_length=100, default="")
-    nextlastgiftdate = models.CharField(max_length=100, default="")
+    lastname = models.CharField(max_length=100, default="", blank=True)
+    phone = models.CharField(max_length=100, default="", blank=True)
+    email = models.CharField(max_length=100, default="", blank=True)
+    paymentnum = models.CharField(max_length=100, default="", blank=True)
+    yeartotal = models.CharField(max_length=100, default="", blank=True)
+    lastgift = models.CharField(max_length=100, default="", blank=True)
+    lastgiftdate = models.CharField(max_length=100, default="", blank=True)
+    nextlastgift = models.CharField(max_length=100, default="", blank=True)
+    nextlastgiftdate = models.CharField(max_length=100, default="", blank=True)
     comments = models.CharField(max_length=100, default="", blank=True)
     def __str__(self):
         return f'{self.orgName} {self.lastname}'
