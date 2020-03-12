a98f2359d01ce03a3868437f2eb79d0dde24d52f
SRBond <skyler.bond7@gmail.com>
2020-03-11 17:22:12 -0700

    change urlpatterns order

--- donor_django/urls.py
+++ donor_django/urls.py
@@ -22,10 +22,10 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('admin/', admin.site.urls),
+    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
     path('', include('donor_app.urls')),
 ]
